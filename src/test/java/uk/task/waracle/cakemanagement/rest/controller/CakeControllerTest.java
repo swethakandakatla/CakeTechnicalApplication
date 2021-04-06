b2a2ac2fd5d617cb3adb4f4ca09f78a005381efc
swetha kandakatla <68458895+swethakandakatla@users.noreply.github.com>
2021-04-06 10:35:18 +0100

    added file

--- src/test/java/uk/task/waracle/cakemanagement/rest/controller/CakeControllerTest.java
+++ src/test/java/uk/task/waracle/cakemanagement/rest/controller/CakeControllerTest.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,17 +22,15 @@ import java.util.Collections;
 public class CakeControllerTest {
 
 	private final CakeService cakeService = Mockito.mock(CakeService.class);
-
-	@InjectMocks
-	private CakeController cakeController;
-
-	@Autowired
 	private MockMvc mockMvc;
+	//Will Conver Json string to java Instance and vice-versa
+	private ObjectMapper objectMapper;
 
 	@BeforeEach  // Earlier JUnit versions will use @Before, but latest JUnit(5), will require BeforeEach
 	public void before() {
-		cakeController = new CakeController(cakeService);
+		CakeController cakeController = new CakeController(cakeService);
 		mockMvc = MockMvcBuilders.standaloneSetup(cakeController).build();
+		objectMapper = new ObjectMapper();
 	}
 
 	@Test
@@ -57,7 +53,6 @@ public class CakeControllerTest {
 
 	@Test
 	public void shouldThrow400ForBadCreateCakeRequest() throws Exception {
-		ObjectMapper objectMapper = new ObjectMapper();
 		String content = objectMapper.writeValueAsString(" { } ");
 
 		MockHttpServletRequestBuilder post = MockMvcRequestBuilders.post("/cake").contentType(MediaType.APPLICATION_JSON)
@@ -66,80 +61,62 @@ public class CakeControllerTest {
 		Assert.assertEquals(400, mvcResult.getResponse().getStatus());
 	}
 
-
 	@Test
-	public void shouldUpdateCakeSuccessfully()throws Exception {
-		
-		try
-		{
-		Cake cake = new Cake("ButterScotch", "WithEgg", 55);
-		Cake savedCake = cakeService.createCake(cake);
-		savedCake.setCakeType("WithFruits");
-		Cake updatedCake = cakeService.updateCakeDetails(savedCake.getId(), savedCake);
-		Mockito.when(cakeService.updateCakeDetails(savedCake.getId(), savedCake)).thenReturn(updatedCake);
-		
-		MockHttpServletRequestBuilder put = MockMvcRequestBuilders.put("/cake/{cakeid}").contentType(MediaType.APPLICATION_JSON);
-		MvcResult mvcResult = mockMvc.perform(put).andReturn();
+	public void shouldUpdateCakeSuccessfully() throws Exception {
+
+		Cake createdCake = new Cake("ButterScotch", "WithEgg", 55);
+		createdCake.setId(20L);
+		Cake updatedCake = new Cake("ButterScotch", "WithResult", 55);
+		Mockito.when(cakeService.updateCakeDetails(Mockito.eq(createdCake.getId()), Mockito.any())).thenReturn(updatedCake);
+
+		String putPayload = objectMapper.writeValueAsString(createdCake);
+		MockHttpServletRequestBuilder putRequest = MockMvcRequestBuilders.put(String.format("/cake/%d", createdCake.getId()))
+			.contentType(MediaType.APPLICATION_JSON).content(putPayload);
+
+		MvcResult mvcResult = mockMvc.perform(putRequest).andReturn();
+
+		Cake result = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Cake.class);
 		Assert.assertEquals(200, mvcResult.getResponse().getStatus());
-		}
-		catch(Exception ex) {
-			
-		}
-		//use mock update successful
-		//assert http status
+		Assert.assertEquals("WithResult", result.getCakeType());
 	}
 
-
 	@Test
-	public void shouldDeleteSuccessfully()throws Exception {
-		try
-		{
+	public void shouldDeleteSuccessfully() throws Exception {
 		Cake cake = new Cake("ButterScotch", "WithEgg", 55);
-		Cake savedCake = cakeService.createCake(cake);
-		long cakeid = savedCake.getId();
-		cakeService.deleteCake(cakeid);
-		MockHttpServletRequestBuilder delete = MockMvcRequestBuilders.delete("/cakes/{cakeid}").contentType(MediaType.APPLICATION_JSON);
+		cake.setId(20L);
+		// Here , CakeService is not a real instance , so mocking deleteCake() method ,
+		// CakeServiceTest does already testing the deleteCake() method
+		Mockito.doNothing().when(cakeService).deleteCake(Mockito.eq(cake.getId()));
+		String deleteUri = String.format("/cakes/%d", cake.getId());
+		MockHttpServletRequestBuilder delete = MockMvcRequestBuilders.delete(deleteUri).contentType(MediaType.APPLICATION_JSON);
 		MvcResult mvcResult = mockMvc.perform(delete).andReturn();
 		Assert.assertEquals(200, mvcResult.getResponse().getStatus());
-		}
-		catch(Exception ex) {
-			
-		}
-		
-   }
+	}
 
 	@Test
-	public void shouldCreateCakeSuccessfully()throws Exception{
-		
+	public void shouldCreateCakeSuccessfully() throws Exception {
+
 		Cake cake = new Cake("vinella", "Eggless", 55);
-		ObjectMapper objectMapper = new ObjectMapper();
-		String content = objectMapper.writeValueAsString(cake);		
+		String content = objectMapper.writeValueAsString(cake);
 		Mockito.when(cakeService.createCake(Mockito.any())).thenReturn(cake);
 		MockHttpServletRequestBuilder post = MockMvcRequestBuilders.post("/cake").contentType(MediaType.APPLICATION_JSON)
 			.content(content);
 		MvcResult mvcResult = mockMvc.perform(post).andReturn();
 		Assert.assertEquals(200, mvcResult.getResponse().getStatus());
-		
-		
+
 	}
 
 	@Test
-	public void shouldThrow404ForNonExistingCake()throws Exception {
-		try
-		{
+	public void shouldThrow404ForNonExistingCake() throws Exception {
 		Exception ex = new ResponseStatusException(HttpStatus.NOT_FOUND, "No Data found!");
 		Cake cake = new Cake("ButterScotch", "WithEgg", 55);
-		Cake savedCake = cakeService.createCake(cake);
-		long cakeid = savedCake.getId();
-		cakeService.deleteCake(cakeid);
-		Mockito.when(cakeService.getCakeById(cakeid)).thenThrow(ex);
-		MockHttpServletRequestBuilder get = MockMvcRequestBuilders.get("/cake/{cakeid}").contentType(MediaType.APPLICATION_JSON);
+		cake.setId(20L);
+		Mockito.when(cakeService.getCakeById(Mockito.eq(20L))).thenThrow(ex);
+
+		String getUri = String.format("/cake/%d",cake.getId());
+		MockHttpServletRequestBuilder get = MockMvcRequestBuilders.get(getUri).contentType(MediaType.APPLICATION_JSON);
 		MvcResult mvcResult = mockMvc.perform(get).andReturn();
 		Assert.assertEquals(404, mvcResult.getResponse().getStatus());
-		}
-		catch(Exception ex) {
-
-		}
 	}
 
 	public CakesListResponse getMockResponse() {
